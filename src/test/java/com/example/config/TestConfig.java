470a96d254e6227c402f083ac6cbae7e0d115762
Edilson Marques <104401369+EdilsonMarquesCurzJr@users.noreply.github.com>
2025-06-14 20:56:41 -0300

    quase

---
+++ src/test/java/com/example/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.example")
+@EntityScan("com.example.model")
+@ActiveProfiles("test")
+public class TestConfig {
+}
